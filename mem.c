43fca5fd63161a68e987e179406a177e5e150453
kiblawi <kiblawi@wisc.edu>
2013-10-11 15:38:44 -0500

    worked on my  methods for mem.c file

--- mem.c
+++ mem.c
@@ -10,11 +10,19 @@
 
 int m_error;
 
-// this is our list structure
+
 typedef struct __list_t{
     int size;
-    void *next;
-} list_t;
+    struct __list_t *next;
+} list_t; // this is our list structure
+
+#define MAX_HEADER_SIZE 16
+
+typedef struct __header_t{
+    int size;
+    int magic;
+}header_t; //header structure for each block of allocated memory
+
 
 //head of list
 list_t *head = NULL;
@@ -31,8 +39,76 @@ Mem_Init(int sizeOfRegion)
 void *
 Mem_Alloc(int size, int style)
 {
-    
-    return NULL;
+    header_t = *hPointer = NULL;
+    list_t *memPointer = NULL;
+    list_t *tmp = head;
+    list_t *parent = NULL;
+    list_t *previousNode = NULL;
+    if style == 0
+    {//BESTFIT
+        bestSize = tmp->size;
+        while(tmp){
+            if (tmp->size > size + MAX_HEADER_SIZE){
+                if(tmp->size <= bestSize){
+                    memPointer = tmp;
+                    bestSize = tmp->size;
+                    if (previousNode != NULL){
+                        parent = previousNode;
+                    }
+                }
+                
+            }
+            previousNode = tmp;
+            tmp = tmp->next;
+        }//end while
+        
+    }//end BESTFIT if
+    else if(style == 1)
+    {// WORST FIT
+        bestSize = 0;
+        while(tmp){
+            if (tmp->size > size + MAX_HEADER_SIZE){
+                if(tmp->size >= bestSize){
+                    memPointer = tmp;
+                    bestSize = tmp->size;
+                    if(previousNode != NULL){
+                        parent = previousNode;
+                    }
+                }
+                
+            }
+            previousNode = tmp;
+            tmp = tmp->next;
+        }//end while
+        
+    }//END WORST FIT
+    else if (style ==2)
+    {// FIRST FIT
+        while(tmp){
+            if (tmp->size > size +MAX_HEADER_SIZE){
+                memPointer = tmp;
+                if(previousNode != NULL){
+                    parent = previousNode;
+                }
+                break;
+            }
+            previousNode = tmp;
+            tmp= tmp->next;
+        }
+        
+    }//END FIRST FIT
+    if memPointer != NULL
+    {
+        if parent == NULL
+        {
+            head = memPointer + size + MAX_HEADER_SIZE;
+        }
+        if parent != NULL
+        {
+            parent->next = memPointer + size + MAX_HEADER_SIZE;
+        }
+    }
+    return (void *)memPointer;
 }
 
 //<--TO-DO-->KRISTIN
