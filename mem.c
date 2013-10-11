0f5050b4a90b9f2325dad88e4359124e41dfc1f2
kiblawi <kiblawi@wisc.edu>
2013-10-11 14:03:57 -0500

    created methods for mem.c file

--- mem.c
+++ mem.c
@@ -10,30 +10,46 @@
 
 int m_error;
 
-//KRISTIN
+// this is our list structure
+typedef struct __list_t{
+    int size;
+    void *next;
+} list_t;
+
+//head of list
+list_t *head = NULL;
+
+//<--TO-DO-->KRISTIN
 int
 Mem_Init(int sizeOfRegion)
 {
     printf("I'm calling : mem_init()");
     return 0;
 }
-//SID
+
+//<--TO-DO-->SID
 void *
 Mem_Alloc(int size, int style)
 {
+    
     return NULL;
 }
 
-//KRISTIN
+//<--TO-DO-->KRISTIN
 int
 Mem_Free(void *ptr)
 {
     return -1;
 }
 
-//SID
+//<--TO-DO-->SID
 void
 Mem_Dump()
 {
-    printf("dump:\n")
+    printf("dump:\n");
+    list_t *tmp = head;
+    while (tmp) {
+        printf("Free Size: %d\n",tmp->size);
+        tmp = tmp->next;
+    }
 }
