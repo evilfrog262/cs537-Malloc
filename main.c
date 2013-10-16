85f2728ca64a772f86a281d25d94d2c80b7e6b30
kiblawi <kiblawi@wisc.edu>
2013-10-16 01:13:45 -0500

    updated main method and mem_alloc appears to be functioning correctly

--- main.c
+++ main.c
@@ -3,16 +3,21 @@
 #include "mem.h"
 
 int main(int argc, char** argv) {
-  //int success;
-  Mem_Init(4096);
-  //success = Mem_Init(-1);
-  /*if (success < 0) {
-    printf("Unsuccessful call to mem_init\n");
-    }*/
-  void *ptr = Mem_Alloc(100, 2);
-  void *ptr2 = Mem_Alloc(100, 2);
-  Mem_Free(ptr);
-  Mem_Free(ptr2);
-  //Mem_Dump();
-  return 0;
+    //int success;
+    Mem_Init(4096);
+    //success = Mem_Init(-1);
+    /*if (success < 0) {
+     printf("Unsuccessful call to mem_init\n");
+     }*/
+    Mem_Dump();
+    void *ptr = Mem_Alloc(100, 2);
+    printf("%p\n",ptr);
+    Mem_Dump();
+    void *ptr2 = Mem_Alloc(100, 2);
+    printf("%p\n",ptr2);
+    Mem_Dump();
+    //Mem_Free(ptr);
+    //Mem_Free(ptr2);
+    //Mem_Dump();
+    return 0;
 }
